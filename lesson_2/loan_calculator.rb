52e1c868317ba55a8bf947c98b20b4ee3090fbf6
Allen Soberano <allensoberano@gmail.com>
2016-10-22 04:54:22 -0700

    psuedo-code loan_calculator

---
+++ lesson_2/loan_calculator.rb
@@ -0,0 +1,33 @@
+Welcome user
+
+get user name
+  - make sure not blank
+
+get loan amount
+  - check for empty or space
+  - ask for amount rounded to nearest dollar
+
+get apr amount
+  - check for empty or space
+  - ask for apr amount 5% 5.25%
+
+get loan duration in years
+  - check for empty or space
+  - ask for apr amount 
+  - (bonus: ask if they know if the apr is by year or by month)
+
+
+calculate monthly interest rate
+calculate loan duration in months
+calculate monthly payment
+Calculate principle vs interest
+
+display Monthly payment broken down into principle and interest
+and overview of princple and interest over the life of the loan.
+
+
+m = p * (j / (1 - (1 + j)**-n))
+m = monthly payment
+p = loan amount
+j = monthly interest rate
+n = loan duration in months
