562db1837db9a8f025ff5a4638b9e3cd0a65fb81
Allen Soberano <allensoberano@gmail.com>
2016-10-25 22:57:04 -0700

    RPS for code review

---
+++ lesson_2/r_p_s_bonus.rb
@@ -0,0 +1,106 @@
+require 'pry'
+require 'yaml'
+
+MESSAGES = YAML.load_file('rps.yml')
+
+VALID_CHOICES = %w(1 2 3 4 5)
+WORD_CHOICES = %w(rock paper scissors lizard spock)
+
+WINNING_WAYS = {  'rock' => %w(scissors lizard),
+                  'paper' => %w(rock spock),
+                  'scissors' => %w(paper lizard),
+                  'lizard' => %w(spock paper),
+                  'spock' => %w(rock scissors) }
+
+def clear_screen
+  system('clear') || system('cls')
+end
+
+def message(message)
+  MESSAGES[message]
+end
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def play_again?
+  prompt(message('play_again'))
+
+  loop do
+    choice = Kernel.gets().chomp()
+    if %(y n).include?(choice.downcase)
+      return choice
+    else
+      prompt(message('invalid_choice'))
+    end
+  end
+end
+
+def convert_choice(choice)
+  WORD_CHOICES[choice.to_i - 1]
+end
+
+def valid_choice?(choice)
+  VALID_CHOICES.include?(choice)
+end
+
+def win?(first_choice, second_choice)
+  WINNING_WAYS[first_choice].include?(second_choice)
+end
+
+def choose_weapon
+  loop do
+    choice = Kernel.gets().chomp()
+    if valid_choice?(choice)
+      return convert_choice(choice)
+    else
+      prompt(message('invalid_choice'))
+    end
+  end
+end
+
+def display_round_winner(player, computer)
+  if player == computer
+    prompt('Tie')
+  elsif win?(player, computer)
+    prompt('You won')
+  else
+    prompt('Computer won')
+  end
+end
+
+# Start of program
+clear_screen
+
+loop do
+  player_choice = ''
+  player_score = 0
+  computer_score = 0
+
+  prompt(message('welcome'))
+
+  loop do
+    prompt(message('select'))
+    player_choice = choose_weapon()
+    computer_choice = convert_choice(VALID_CHOICES.sample)
+
+    clear_screen
+
+    select = message('chosen')
+    prompt(format(select, p_choice: player_choice, c_choice: computer_choice))
+
+    display_round_winner(player_choice, computer_choice)
+
+    player_score += 1 if win?(player_choice, computer_choice)
+    computer_score += 1 if win?(computer_choice, player_choice)
+
+    score_mess = message('score')
+    prompt(format(score_mess, p_score: player_score, c_score: computer_score))
+
+    prompt(message('game_over'))
+    break if player_score == 5 || computer_score == 5
+  end
+
+  break unless play_again?() == 'y'
+end
--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,17 +1,26 @@
-VALID_CHOICES = %w(rock paper scissors)
+require 'pry'
 
-def test_method
-  prompt('test message')
-end
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+
+WINNING_WAYS = {  'rock' => %w(scissors, lizard),
+                  'paper' => %w(rock, spock),
+                  'scissors' => %w(paper, lizard),
+                  'lizard' => %w(spock, paper),
+                  'spock' => %w(rock, scissors)
+                }
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  WINNING_WAYS[first].include?(second)
+
+  # (first == 'rock' && (second == 'scissors' || second == 'lizard')) ||
+  #   (first == 'paper' && (second == 'rock' || second == 'spock')) ||
+  #   (first == 'scissors' && (second == 'paper' || second == 'lizard')) ||
+  #   (first == 'lizard' && (second == 'spock' || second == 'paper')) ||
+  #   (first == 'spock' && (second == 'rock' || second == 'scissors'))
 end
 
 def display_results(player, computer)
@@ -24,29 +33,68 @@ def display_results(player, computer)
   end
 end
 
+def convert_abbr(choice)
+  case choice
+  when 'r'
+    return 'rock'
+  when 'p'
+    return 'paper'
+  when 's'
+    return 'scissors'
+  when 'l'
+    return 'lizard'
+  when 'sp'
+    return 'spock'
+  end
+end
+
 loop do
-  choice = ''
+  player_score = 0
+  computer_score = 0
 
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+    player_choice = ''
 
-    if VALID_CHOICES.include?(choice)
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      prompt("You can use: r, p, s, l or sp")
+
+      player_choice = Kernel.gets().chomp()
+
+      player_choice = convert_abbr(player_choice) if player_choice.length <= 2
+
+      if VALID_CHOICES.include?(player_choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
+    end
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You chose: #{player_choice}, Computer chose: #{computer_choice}")
+
+    puts win?(player_choice, computer_choice)
+    binding.pry
+
+    display_results(player_choice, computer_choice)
+
+    player_score += 1 if win?(player_choice, computer_choice)
+    computer_score += 1 if win?(computer_choice, player_choice)
+
+    prompt("Player Score #{player_score}, Computer Score: #{computer_score}")
+
+    if player_score == 5
+      prompt("Congrats! You won")
       break
-    else
-      prompt("That's not a valid choice.")
+    elsif computer_score == 5
+      prompt("Sorry, the computer won.")
+      break
     end
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  prompt("You chose: #{choice}, Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice)
-
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  prompt("Do you want to play again? Y or N")
+    answer = Kernel.gets().chomp()
+    break unless answer.downcase().start_with?('y')
 end
 
 prompt("Thank you for playing! Good Bye.")
